022bdf72580b4d20b61d5c2d13c952392a5b3146
ricky <2946677871@qq.com>
2023-03-13 00:46:58 +0800

    添加左旋右旋操作

--- Tree/rbtree.c
+++ Tree/rbtree.c
@@ -18,3 +18,59 @@ typedef struct _rbtree
     rbtree_node *root;
     rbtree_node *nil;
 } rbtree;
+
+void _left_rotate(rbtree *T, rbtree_node *x)
+{
+    rbtree_node *y = x->right;
+
+    x->right = y->left;
+    if (y->left != T->nil)
+    {
+        y->left->parent = x;
+    }
+
+    y->parent = x->parent;
+    if (x->parent == T->nil)
+    {
+        T->root == y;
+    }
+    else if (x == x->parent->left)
+    {
+        x->parent->left = y;
+    }
+    else if (x == x->parent->right)
+    {
+        x->parent->right = y;
+    }
+
+    y->left = x;
+    x->parent = y;
+}
+
+void _right_rotate(rbtree *T, rbtree_node *y)
+{
+    rbtree_node *x = y->left;
+
+    y->left = x->right;
+    if (x->right != T->nil)
+    {
+        x->right->parent = y;
+    }
+
+    x->parent = y->parent;
+    if (y->parent == T->nil)
+    {
+        T->root = x;
+    }
+    else if (y == y->parent->left)
+    {
+         y->parent->left = x;
+    }
+    else if (y == y->parent->right)
+    {
+        y->parent->right = x;
+    }
+
+    x->right = y;
+    y->parent = x;
+}
