28ec1acac54c1aeaa714222dbdf792ebc2fdb812
ricky <2946677871@qq.com>
2023-03-18 23:56:15 +0800

    添加模板方法的继承子类

--- DesignPatterns/template.cpp
+++ DesignPatterns/template.cpp
@@ -29,6 +29,37 @@ protected:
     }
 };
 
+class PerformEx1 : public Perform {
+protected:
+    virtual bool show0() {
+        cout << "ex1 show0" << endl;
+        return true;
+    }
+    virtual void show2() {
+        cout << "ex1 show2" << endl;
+    }
+};
+
+class PerformEx2 : public Perform {
+protected:
+    virtual void show1() {
+        cout << "ex2 show1" << endl;
+    }
+    virtual void show2() {
+        cout << "ex2 show2" << endl;
+    }
+};
+
+class PerformEx3 : public Perform {
+protected:
+    virtual void show2() {
+        cout << "ex3 show2" << endl;
+    }
+    virtual void show3() {
+        cout << "ex3 show3" << endl;
+    }
+};
+
 int main()
 {
     return 0;
