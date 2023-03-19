a41745913fc1b599841a7c9037252ac752eeceb1
ricky <2946677871@qq.com>
2023-03-19 23:48:59 +0800

    添加观察者模式子类定义

--- DesignPatterns/observer.cpp
+++ DesignPatterns/observer.cpp
@@ -10,6 +10,27 @@ public:
     virtual ~IDisplay() {}
 };
 
+class DisplayA : public IDisplay
+{
+public:
+    virtual void show(float temperature);
+
+private:
+    void ownFunction(); // 可以在子类上继续添加自己需要的函数
+};
+
+class DisplayB : public IDisplay
+{
+public:
+    virtual void show(float temperature);
+};
+
+class DisplayC : public IDisplay
+{
+public:
+    virtual void show(float temperature);
+};
+
 int main()
 {
     return 0;
