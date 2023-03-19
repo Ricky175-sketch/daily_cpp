6b7c621602ab29ffd29e1b549747329e18f030ef
ricky <2946677871@qq.com>
2023-03-19 23:55:34 +0800

    添加观察者模式数据中心

--- DesignPatterns/observer.cpp
+++ DesignPatterns/observer.cpp
@@ -31,6 +31,34 @@ public:
     virtual void show(float temperature);
 };
 
+class WeatherData
+{
+
+};
+
+class DataCenter
+{
+public:
+    bool attach(IDisplay *ob);
+    bool detach(IDisplay *ob);
+    void notify()
+    {
+        float temperature = CalcTemperature();
+        for (auto iter = this->obs.begin(); iter != obs.end(); iter++)
+            (*iter)->show(temperature);
+    }
+private:
+    virtual WeatherData *GetWeatherData();
+    virtual float CalcTemperature()
+    {
+        WeatherData *data = GetWeatherData();
+        // ...
+        float temperature;
+        return temperature;
+    }
+    std::vector<IDisplay *> obs;
+};
+
 int main()
 {
     return 0;
