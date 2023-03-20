d6a48405c35e16d4dce2616c172fa335db72159d
ricky <2946677871@qq.com>
2023-03-21 00:14:01 +0800

    添加策略模式具体策略类的实现

--- DesignPatterns/strategy.cpp
+++ DesignPatterns/strategy.cpp
@@ -4,14 +4,41 @@ using namespace std;
 
 class Context
 {
-
+    // 环境类
 };
 
 class ProStrategy
 {
+    // 抽象的策略类
 public:
+    // 算法接口
     virtual double CalcPro(const Context &ctx) = 0;
     virtual ~ProStrategy();
+};
+
+// 派生出的具体策略类
+class VAC_Spring : public ProStrategy {
+public:
+    virtual double CalcPro(const Context &ctx){}
+};
+
+class VAC_Labor : public ProStrategy {
+public:
+    virtual double CalcPro(const Context &ctx){}
+};
+class VAC_Labor1  : public VAC_Labor {
+public:
+    virtual double CalcPro(const Context &ctx){}
+};
+
+class VAC_National : public ProStrategy {
+public:
+    virtual double CalcPro(const Context &ctx){}
+};
+
+class VAC_Christmas : public ProStrategy {
+public:
+    virtual double CalcPro(const Context &ctx){}
 };
 
 class Promotion
