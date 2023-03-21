059001de960091f630a4b9251b918ab35e370d46
ricky <2946677871@qq.com>
2023-03-21 11:18:47 +0800

    添加单列模式版本二，解决多线程安全问题

--- DesignPatterns/singleton.cpp
+++ DesignPatterns/singleton.cpp
@@ -16,7 +16,38 @@
 // };
 // Singleton *Singleton::_instance = nullptr;
 
+// #include <cstdlib>
+// class Singleton
+// {
+// public:
+//     static Singleton *getInstance()
+//     {
+//         if (_instance == nullptr)
+//         {
+//             _instance = new Singleton();
+//             atexit(destructor); // 当进程退出时调用destructor，完成堆区内存的释放
+//         }
+//         return _instance;
+//     }
+// private:
+//     static void destructor()
+//     {
+//         if (_instance != nullptr)
+//         {
+//             delete _instance;
+//             _instance = nullptr;
+//         }
+//     }
+//     Singleton() {}
+//     ~Singleton() {}
+//     Singleton(const Singleton &clone) {} // 禁用拷贝构造
+//     Singleton &operator=(const Singleton &s) {} // 禁用赋值
+//     static Singleton *_instance;
+// };
+// Singleton *Singleton::_instance = nullptr;
+
 #include <cstdlib>
+#include <mutex>
 class Singleton
 {
 public:
@@ -24,8 +55,12 @@ public:
     {
         if (_instance == nullptr)
         {
-            _instance = new Singleton();
-            atexit(destructor); // 当进程退出时调用destructor，完成堆区内存的释放
+            std::lock_guard<std::mutex> lock(_mutex); // 因为全局的_instance只需要初始化一次，故在判断_instance不为空之后再加锁，避免多次加锁，造成性能损失
+            if (_instance == nullptr) // 再次判断_instance不为空，避免多个线程同时到达进行加锁，_instance初始化多次
+            {
+                _instance = new Singleton();
+                atexit(destructor); // 当进程退出时调用destructor，完成堆区内存的释放
+            }
         }
         return _instance;
     }
@@ -43,5 +78,7 @@ private:
     Singleton(const Singleton &clone) {} // 禁用拷贝构造
     Singleton &operator=(const Singleton &s) {} // 禁用赋值
     static Singleton *_instance;
+    static std::mutex _mutex;
 };
 Singleton *Singleton::_instance = nullptr;
+std::mutex Singleton::_mutex; // 初始化互斥锁
