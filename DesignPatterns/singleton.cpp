60b200ecf17804b251337642b581b33fbb35a31e
ricky <2946677871@qq.com>
2023-03-21 12:40:39 +0800

    添加单例模式版本五，使用C++11新特性magic static简化代码

--- DesignPatterns/singleton.cpp
+++ DesignPatterns/singleton.cpp
@@ -83,45 +83,61 @@
 // Singleton *Singleton::_instance = nullptr;
 // std::mutex Singleton::_mutex; // 初始化互斥锁
 
-#include <cstdlib>
-#include <mutex>
-#include <atomic>
+// #include <cstdlib>
+// #include <mutex>
+// #include <atomic>
+// class Singleton
+// {
+// public:
+//     static Singleton *getInstance()
+//     {
+//         Singleton *tmp = _instance.load(std::memory_order_relaxed);
+//         std::atomic_thread_fence(std::memory_order_acquire); // 获取内存屏障
+//         if (tmp == nullptr)
+//         {
+//             std::lock_guard<std::mutex> lock(_mutex); // 因为全局的_instance只需要初始化一次，故在判断_instance不为空之后再加锁，避免多次加锁，造成性能损失
+//             tmp = _instance.load(std::memory_order_relaxed);
+//             if (tmp == nullptr) // 再次判断_instance不为空，避免多个线程同时到达进行加锁，_instance初始化多次
+//             {
+//                 tmp = new Singleton;
+//                 std::atomic_thread_fence(std::memory_order_release); // 释放内存屏障
+//                 _instance.store(tmp, std::memory_order_relaxed);
+//                 atexit(destructor);
+//             }
+//         }
+//         return tmp;
+//     }
+// private:
+//     static void destructor()
+//     {
+//         if (_instance != nullptr)
+//         {
+//             delete _instance;
+//             _instance = nullptr;
+//         }
+//     }
+//     Singleton() {}
+//     ~Singleton() {}
+//     Singleton(const Singleton &clone) {} // 禁用拷贝构造
+//     Singleton &operator=(const Singleton &s) {} // 禁用赋值
+//     static std::atomic<Singleton*> _instance;
+//     static std::mutex _mutex;
+// };
+// std::atomic<Singleton*> Singleton::_instance;
+// std::mutex Singleton::_mutex; // 初始化互斥锁
+
+// C++11 magic static特性：如果当变量在初始化的时候，并发同时进入声明语句，并发线程将会阻塞等待初始化结束
 class Singleton
 {
 public:
-    static Singleton *getInstance()
+    static Singleton & getInstance()
     {
-        Singleton *tmp = _instance.load(std::memory_order_relaxed);
-        std::atomic_thread_fence(std::memory_order_acquire); // 获取内存屏障
-        if (tmp == nullptr)
-        {
-            std::lock_guard<std::mutex> lock(_mutex); // 因为全局的_instance只需要初始化一次，故在判断_instance不为空之后再加锁，避免多次加锁，造成性能损失
-            tmp = _instance.load(std::memory_order_relaxed);
-            if (tmp == nullptr) // 再次判断_instance不为空，避免多个线程同时到达进行加锁，_instance初始化多次
-            {
-                tmp = new Singleton;
-                std::atomic_thread_fence(std::memory_order_release); // 释放内存屏障
-                _instance.store(tmp, std::memory_order_relaxed);
-                atexit(destructor);
-            }
-        }
-        return tmp;
+        static Singleton instance;
+        return instance;
     }
 private:
-    static void destructor()
-    {
-        if (_instance != nullptr)
-        {
-            delete _instance;
-            _instance = nullptr;
-        }
-    }
     Singleton() {}
     ~Singleton() {}
-    Singleton(const Singleton &clone) {} // 禁用拷贝构造
-    Singleton &operator=(const Singleton &s) {} // 禁用赋值
-    static std::atomic<Singleton*> _instance;
-    static std::mutex _mutex;
+    Singleton(const Singleton &) {}
+    Singleton& operator=(const Singleton &) {}
 };
-std::atomic<Singleton*> Singleton::_instance;
-std::mutex Singleton::_mutex; // 初始化互斥锁
