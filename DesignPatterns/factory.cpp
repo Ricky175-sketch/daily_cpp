41387cffea7caabe82dd913b48ef305108a63649
ricky <2946677871@qq.com>
2023-03-22 09:48:14 +0800

    添加工厂模式导出数据接口的实现子类

--- DesignPatterns/factory.cpp
+++ DesignPatterns/factory.cpp
@@ -8,3 +8,30 @@ public:
     virtual ~IExport() {}
 };
 
+class ExportXml : public IExport {
+public:
+    virtual bool export(const std::string &data) {
+        return true;
+    }
+};
+
+class ExportJson : public IExport {
+public:
+    virtual bool export(const std::string &data) {
+        return true;
+    }
+};
+
+class ExportTxt : public IExport {
+public:
+    virtual bool export(const std::string &data) {
+        return true;
+    }
+};
+
+class ExportCSV : public IExport {
+public:
+    virtual bool export(const std::string &data) {
+        return true;
+    }
+};
