62f403f4c758243998ee05585d6a114273310ad7
ricky <2946677871@qq.com>
2023-03-22 21:12:16 +0800

    添加抽象工厂数据导出导入工厂类

--- DesignPatterns/abstractFactory.cpp
+++ DesignPatterns/abstractFactory.cpp
@@ -87,6 +87,47 @@ public:
     }
 };
 
+class IDataApiFactory
+{
+public:
+    IDataApiFactory()
+    {
+        this->_export = nullptr;
+        this->_import = nullptr;
+    }
+    virtual ~IDataApiFactory()
+    {
+        if (this->_export != nullptr)
+        {
+            delete this->_export;
+            this->_export = nullptr;
+        }
+        if (this->_import != nullptr)
+        {
+            delete this->_import;
+            this->_import = nullptr;
+        }
+    }
+    bool export(const std::string &data)
+    {
+        if (this->_export == nullptr)
+            this->_export = this->newExport();
+        return this->_export->export(data);
+    }
+    bool import(const std::string &data)
+    {
+        if (this->_import == nullptr)
+            this->_import = this->newImport();
+        return this->_import->import(data);
+    }
+protected:
+    virtual IExport *newExport(/* 可以添加自定义参数 */) = 0;
+    virtual IImport *newImport(/* 可以添加自定义参数 */) = 0;
+private:
+    IExport *_export;
+    IImport *_import;
+};
+
 int main()
 {
     return 0;
