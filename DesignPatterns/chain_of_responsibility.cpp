bf5b9531b166df1d82144998ac221c4610e44c2a
ricky <2946677871@qq.com>
2023-03-22 23:30:46 +0800

    添加责任链处理链条上的各个处理流程

--- DesignPatterns/chain_of_responsibility.cpp
+++ DesignPatterns/chain_of_responsibility.cpp
@@ -36,3 +36,47 @@ protected:
 private:
     IHandler *next;
 };
+
+class HandleByMainProgram : public IHandler
+{
+protected:
+    virtual bool HandleRequest(const Context &ctx)
+    {
+        // 自己的处理逻辑
+        return true;
+    }
+    virtual bool CanHandle(const Context &ctx)
+    {
+        if (ctx.day <= 10)
+            return true;
+        return false;
+    }
+};
+
+class HandleByProjMgr : public IHandler {
+protected:
+    virtual bool HandleRequest(const Context &ctx)
+    {
+        return true;
+    }
+    virtual bool CanHandle(const Context &ctx)
+    {
+        if (ctx.day <= 20)
+            return true;
+        return false;
+    }
+};
+
+class HandleByBoss : public IHandler {
+protected:
+    virtual bool HandleRequest(const Context &ctx)
+    {
+        return true;
+    }
+    virtual bool CanHandle(const Context &ctx)
+    {
+        if (ctx.day < 30)
+            return true;
+        return false;
+    }
+};
