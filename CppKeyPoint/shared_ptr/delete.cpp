74dc766d3b787935071266e0ffdc160e5c96cc40
ricky <2946677871@qq.com>
2023-03-23 22:58:03 +0800

    添加智能指针shared_ptr指定删除器

---
+++ CppKeyPoint/shared_ptr/delete.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <memory>
+
+void Destructor(int *p)
+{
+    std::cout << "call destructor function" << std::endl;
+    delete p;
+}
+
+int main()
+{
+    std::shared_ptr<int> p1(new int(10), Destructor);
+
+    std::shared_ptr<int> p2(new int(100), [](int *p) {
+        std::cout << "cal lambda destructor function" << std::endl;
+        delete p;
+    });
+
+    return 0;
+}
