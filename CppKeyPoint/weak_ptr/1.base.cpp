b5ade4c873865da3be09ad7406630aa21ae17c44
ricky <2946677871@qq.com>
2023-03-24 23:40:20 +0800

    添加智能指针weak_ptr的expired函数

--- CppKeyPoint/weak_ptr/1.base.cpp
+++ CppKeyPoint/weak_ptr/1.base.cpp
@@ -4,9 +4,13 @@
 int main()
 {
     std::shared_ptr<int> p(new int(1));
-    std::shared_ptr<int> pp = p;
+    // std::shared_ptr<int> pp = p;
     std::weak_ptr<int> wp(p);
-    std::cout << wp.use_count() << std::endl;
+    // std::cout << wp.use_count() << std::endl;
+
+    std::cout << wp.expired() << std::endl;
+    p.reset();
+    std::cout << wp.expired() << std::endl;
 
     return 0;
 }
