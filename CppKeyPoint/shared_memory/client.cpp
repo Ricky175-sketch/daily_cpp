c9e4c6aa90ac3b9b21a76fb08ec3c07a122298e1
ricky <2946677871@qq.com>
2023-03-27 14:02:18 +0800

    添加共享内存的读取端

---
+++ CppKeyPoint/shared_memory/client.cpp
@@ -0,0 +1,37 @@
+#include <Windows.h>
+#include <iostream>
+
+int main()
+{
+    // 打开共享内存
+    HANDLE hMapFile = OpenFileMapping(
+        FILE_MAP_ALL_ACCESS,     // 可读可写
+        FALSE,                   // 不继承句柄
+        L"MySharedMemory");      // 共享内存名称
+
+    if (hMapFile == NULL)
+    {
+        std::cerr << "Failed to open shared memory!" << std::endl;
+        return 1;
+    }
+
+    // 映射共享内存
+    LPVOID lpBase = MapViewOfFile(
+        hMapFile,                // 共享内存句柄
+        FILE_MAP_ALL_ACCESS,     // 可读可写
+        0,                       // 偏移量（高位）
+        0,                       // 偏移量（低位）
+        1024);                   // 映射内存大小
+
+    // 读取共享内存
+    char* pData = (char*)lpBase;
+    std::cout << pData << std::endl;
+
+    // 解除共享内存映射
+    UnmapViewOfFile(lpBase);
+
+    // 关闭共享内存句柄
+    CloseHandle(hMapFile);
+
+    return 0;
+}
