ba5ea56c83e3767930cb50bb34ada3fab8d3512b
ricky <2946677871@qq.com>
2023-04-05 17:05:07 +0800

    添加线程创建并传递参数

--- CppKeyPoint/thread/1.create.cpp
+++ CppKeyPoint/thread/1.create.cpp
@@ -8,7 +8,11 @@ int main()
     });
     t1.join();
 
-
+    std::thread t2([] (int a, int b) {
+        std::cout << "Hello from thread " << std::this_thread::get_id() << std::endl;
+        std::cout << "a + b = " << a + b << std::endl;
+    }, 1, 2);
+    t2.join();
 
     return 0;
 }
